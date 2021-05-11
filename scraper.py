dff1639f8487a3e23089874559af47e205a66583
Mone Duncans-Francis <mone.eunique12@gmail.com>
2021-05-10 20:24:24 -0700

    scraping data in python

--- scraper.py
+++ scraper.py
@@ -1,13 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-# set the url to perform the get request
-URL = 'https://science.nasa.gov/science-news'
-page = requests.get(URL)
 
-# load the page content
-text = page.content
+# set the url to perform the get request
+URL = requests.get('https://science.nasa.gov/science-news')
 
 # make a soup object by using beautiful
 # soup and set the markup as html parser
-soup = BeautifulSoup(text, "html.parser")
+soup = BeautifulSoup(URL.content, "html.parser")
+
+
+titles = []
+images = []
+descriptions = []
+
+t_results = soup.find_all('span', class_='views-field-title')
+
+for tag in t_results:
+    title = tag.find('a')
+    titles.append(title)
+print(titles)
+
+d_results = soup.find_all('span', class_='field-content')
+
+for text in d_results[1:]:
+    descript = text.get_text()
+    descriptions.append(descript)
+print(descriptions)
